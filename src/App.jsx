d133da383dba4a9baebdaa431b665d4668ec2694
Swathi <machireddyswathi90@gmail.com>
2026-04-02 16:04:22 +0530

    Fix routing issue

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,16 @@
 import React from "react";
 import Navbar from "./components/Navbar/Navbar.jsx";
 import Sidebar from "./components/Sidebar/Sidebar.jsx";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import Orders from "./pages/Orders/Orders.jsx";
 import List from "./pages/List/List.jsx";
 import Add from "./pages/Add/Add.jsx";
-//for notifications
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+
 const App = () => {
-  const url="https://food-delivery-backend-9k56.onrender.com"
+  const url = "https://food-delivery-backend-9k56.onrender.com";
+
   return (
     <div>
       <ToastContainer />
@@ -18,9 +19,12 @@ const App = () => {
       <div className="app-content">
         <Sidebar />
         <Routes>
-          <Route path="/add" element={<Add url={url}/>} />
-          <Route path="/list" element={<List url={url}/>} />
-          <Route path="/Orders" element={<Orders url={url}/>} />
+          {/* ✅ Default route */}
+          <Route path="/" element={<Navigate to="/add" />} />
+
+          <Route path="/add" element={<Add url={url} />} />
+          <Route path="/list" element={<List url={url} />} />
+          <Route path="/orders" element={<Orders url={url} />} />
         </Routes>
       </div>
     </div>
